a07cccacb55110d4fe43ebed0deb671b3a3bb1bf
Alexander Daum <alexander.daum@mailbox.org>
2022-10-13 14:56:01 +0200

    Add helper functions and defines
    Function set_led sets the PWM value for one LED and checks parameters
    Defines for Number of Channels and Maximum value

--- drivers/misc/ledpwm.c
+++ drivers/misc/ledpwm.c
@@ -12,9 +12,12 @@
 #include <linux/io.h>
 
 #define PWM_REG_BASE 0xFF203080UL
-#define PWM_REG_SIZE (10 * sizeof(uint32_t))
+#define PWM_NUM_CHANNELS 10
+#define PWM_REG_SIZE (PWM_NUM_CHANNELS * sizeof(uint32_t))
 
-static void __iomem *led_regs;
+#define PWM_MAX 0x7FFU
+
+static void __iomem *led_regs = NULL;
 
 /**
  * Requests and maps the PWM I/O registers.
@@ -29,12 +32,13 @@ static int map_registers(void)
 
 	if (!request_mem_region(PWM_REG_BASE, PWM_REG_SIZE, "PWM Regs")) {
 		printk(KERN_ERR "Unable to request mem region\n");
-		return EBUSY;
+		return -EBUSY;
 	}
 
 	led_regs = ioremap(PWM_REG_BASE, PWM_REG_SIZE);
 	if (!led_regs) {
 		printk(KERN_ERR "Unable to ioremap PWM Registers\n");
+		ret = -EBUSY;
 		goto release;
 	}
 
@@ -47,9 +51,30 @@ end:
 	return ret;
 }
 
+/**
+ * Set the value (brightness) of a single LED PWM Channel
+ * @param ledNr Index of the LED, must be between 0 and PWM_NUM_CHANNELS
+ * @param value Duty Cycle of the PWM, must be between 0 and PWM_MAX
+ * @return -EINVAL if ledNr or value are invalid, 0 otherwise
+ */
+static int set_led(int ledNr, uint32_t value)
+{
+	if (ledNr < 0 || ledNr >= PWM_NUM_CHANNELS) {
+		return -EINVAL;
+	}
+	if (value > PWM_MAX) {
+		return -EINVAL;
+	}
+
+	iowrite32(value, led_regs + ledNr * sizeof(uint32_t));
+
+	return 0;
+}
+
 static int __init led_pwm_init(void)
 {
 	int ret = 0;
+	int i;
 
 	printk(KERN_INFO "Loading PWM Driver\n");
 	ret = map_registers();
@@ -57,18 +82,24 @@ static int __init led_pwm_init(void)
 		return ret;
 	}
 
-	iowrite32(0x7FF, led_regs);
+	for (i = 0; i < 8; i++) {
+		set_led(i, PWM_MAX);
+	}
 
 	return ret;
 }
 
 static void __exit led_pwm_exit(void)
 {
+	int i;
 	printk(KERN_INFO "Unloading PWM Driver\n");
 
-	iowrite32(0x00, led_regs);
+	for (i = 0; i < PWM_NUM_CHANNELS; i++) {
+		set_led(i, 0);
+	}
 
 	iounmap(led_regs);
+	led_regs = NULL;
 	release_mem_region(PWM_REG_BASE, PWM_REG_SIZE);
 }
 
