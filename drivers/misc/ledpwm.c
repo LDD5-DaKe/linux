61f0181c392f6d61db71dc622b53fc79e57e4d3c
Matthias Kern <kern_matthias@gmx.at>
2022-10-19 19:18:58 +0200

    Implement the running light
    Use <linux/timer.h> to get timing control.
    After initialization the LEDs will stay at full brightness for approx. 3
    seconds, afterwards a running light with trail will get started, it
    changes with a frequency of 8 Hz.
    
    Signed-off-by: Matthias Kern <kern_matthias@gmx.at>

--- drivers/misc/ledpwm.c
+++ drivers/misc/ledpwm.c
@@ -10,6 +10,8 @@
 #include <linux/init.h>
 #include <linux/ioport.h>
 #include <linux/io.h>
+#include <linux/jiffies.h>
+#include <linux/timer.h>
 
 #define PWM_REG_BASE 0xFF203080UL
 #define PWM_NUM_CHANNELS 10
@@ -17,7 +19,20 @@
 
 #define PWM_MAX 0x7FFU
 
+#define NUM_LEDS 8
+#define STARTUP_DELAY 3000
+#define TIMER_DELAY 125
+
 static void __iomem *led_regs = NULL;
+
+/**
+ * callback to execute when the timer elapses
+ * this function will create the pattern on the leds
+ *
+ */
+static void on_timer_elapsed(struct timer_list *timer);
+
+static DEFINE_TIMER(timer, on_timer_elapsed);
 
 /**
  * Requests and maps the PWM I/O registers.
@@ -59,16 +74,35 @@ end:
  */
 static int set_led(int ledNr, uint32_t value)
 {
-	if (ledNr < 0 || ledNr >= PWM_NUM_CHANNELS) {
+	if (ledNr < 0 || ledNr >= PWM_NUM_CHANNELS)
 		return -EINVAL;
-	}
-	if (value > PWM_MAX) {
+
+	if (value > PWM_MAX)
 		return -EINVAL;
-	}
 
 	iowrite32(value, led_regs + ledNr * sizeof(uint32_t));
 
 	return 0;
+}
+
+void on_timer_elapsed(struct timer_list *timer)
+{
+	static u32 currPos; // will be initialized to 0
+
+	// create the pattern for a running light
+	u32 i = currPos;
+	u32 currVal = PWM_MAX;
+
+	// create a running led with a slight trail
+	do {
+		set_led(i, currVal);
+		// currVal will be 0 after 3 loops
+		currVal >>= 4;
+		i = (i - 1) % NUM_LEDS;
+	} while (i != currPos);
+
+	currPos = (currPos + 1) % NUM_LEDS;
+	mod_timer(timer, jiffies + msecs_to_jiffies(TIMER_DELAY));
 }
 
 static int __init led_pwm_init(void)
@@ -78,13 +112,14 @@ static int __init led_pwm_init(void)
 
 	printk(KERN_INFO "Loading PWM Driver\n");
 	ret = map_registers();
-	if (ret != 0) {
+	if (ret != 0)
 		return ret;
-	}
 
-	for (i = 0; i < 8; i++) {
+	for (i = 0; i < NUM_LEDS; i++)
 		set_led(i, PWM_MAX);
-	}
+
+	// configure the timer, first callback after 3000 ms
+	mod_timer(&timer, jiffies + msecs_to_jiffies(STARTUP_DELAY));
 
 	return ret;
 }
@@ -94,9 +129,11 @@ static void __exit led_pwm_exit(void)
 	int i;
 	printk(KERN_INFO "Unloading PWM Driver\n");
 
-	for (i = 0; i < PWM_NUM_CHANNELS; i++) {
+	// stop the timer -> otherwise kernel panic :-)
+	del_timer_sync(&timer);
+
+	for (i = 0; i < PWM_NUM_CHANNELS; i++)
 		set_led(i, 0);
-	}
 
 	iounmap(led_regs);
 	led_regs = NULL;
@@ -109,3 +146,4 @@ module_exit(led_pwm_exit);
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Driver for the LED PWM component of the DE1-SoC Computer");
 MODULE_AUTHOR("Alexander Daum <alexander.daum@mailbox.org>");
+MODULE_AUTHOR("Matthias Kern <kern_matthias@gmx.at>");
